1caa977504a6908eae5d23b8fb2c62fd34f643ff
manjula <makelamn12@gmail.com>
2022-06-30 23:26:33 +0100

    bbcommit

--- src/main/java/com/example/ProductCatelogue/controller/ProdCatController.java
+++ src/main/java/com/example/ProductCatelogue/controller/ProdCatController.java
@@ -37,7 +37,7 @@ public class ProdCatController {
 	@DeleteMapping(value = "get/delete/{id}")
 	public String delResource(@PathVariable("id") int proId){
 		productCatService.delResource(proId);
-		return "deleted prod1 resource sucessfully";
+		return "deleted prod2 resource sucessfully";
 	}
 		
 	@GetMapping("/get/{id}")
